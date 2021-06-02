6cb970e7728d505ff0e74d7bb733d028fcc78ebf
Susana <susana.santos@tajamar365.com>
2021-06-02 14:09:06 +0200

    diseño de detalle a medias

--- SocksMobile/SocksMobile/ViewModels/SocksDetailsViewModel.cs
+++ SocksMobile/SocksMobile/ViewModels/SocksDetailsViewModel.cs
@@ -4,6 +4,7 @@ using SocksMobile.Services;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 
@@ -16,6 +17,9 @@ namespace SocksMobile.ViewModels
         public SocksDetailsViewModel(ServiceSocks service)
         {
             this.ServiceSocks = service;
+            Task.Run(async () => {
+                this.Sizes = await this.ServiceSocks.GetProduct_SizesByProductAsync(this.Product.Product_id);
+            });
         }
 
         private Product_Complete _Product;
@@ -26,6 +30,16 @@ namespace SocksMobile.ViewModels
             {
                 this._Product = value;
                 OnPropertyChanged("Product");
+            }
+        }
+
+
+        private List<Product_sizes> _Sizes;
+        public List<Product_sizes> Sizes {
+            get { return this._Sizes; }
+            set {
+                this._Sizes = value;
+                OnPropertyChanged("Sizes");
             }
         }
     }
